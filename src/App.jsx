71baa9bb5855511a38120c8325b6547561253894
Kevblakebo <kevinblakeborough@gmail.com>
2026-08-20 09:08:47 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -177,7 +177,7 @@ const GAMES = {
   },
   moonlightwolf: {
     name: "Wolf",
-    tag: "Rotating team - 4 players",
+    tag: "Rotating teams - players",
     desc: "A rotating team four-player game where one player acts as the \"Wolf\" on each hole. The Wolf watches the other drives (tees off last), then either chooses a partner or goes it alone (\"Lone Wolf\"), to win points based on a best-ball lowest strokes and putts format.",
     rotates: true,
     hasScore: true,
