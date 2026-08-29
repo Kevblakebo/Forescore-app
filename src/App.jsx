8cb2565d84ab719cbda610b472af3b1d858785a2
Kevblakebo <kevinblakeborough@gmail.com>
2026-08-29 07:42:45 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5359,7 +5359,7 @@ function computeRoundScoring(round) {
       <div className="gsc">
         <style>{STYLE}</style>
         <Header
-          title={<><span style={{ fontSize: 23 }}>RipScore Golf</span><div style={{ fontSize: 11, fontWeight: 400, opacity: 0.75, letterSpacing: "0.5px", textTransform: "uppercase", marginTop: 2, fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif" }}>Bragging rights, automatically tracked.</div></>}
+          title={<><span style={{ fontSize: 23 }}>RipScore Golf</span><div style={{ fontSize: 11, fontWeight: 400, opacity: 0.75, letterSpacing: "0.5px", textTransform: "uppercase", marginTop: 2, fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif" }}>Your crew. Your leaderboard.</div></>}
           sub=""
         />
         <div className="gsc-body gsc-body-tabbed">
